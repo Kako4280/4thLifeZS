a7181fefd21f2a9735bbca5fed5e32404836dc54
Lightweasel <51730197+Lightweasel@users.noreply.github.com>
2021-10-02 09:24:12 -0700

    fix defense buff causing lua errors

--- gamemodes/zombiesurvival/entities/entities/status_medrifledefboost/init.lua
+++ gamemodes/zombiesurvival/entities/entities/status_medrifledefboost/init.lua
@@ -22,11 +22,13 @@ function ENT:EntityTakeDamage(ent, dmginfo)
 		dmginfo:SetDamage(dmginfo:GetDamage() * 0.7)
 
 		local points = dmginfo:GetDamage() * 0.015
+		if self.Applier and self.Applier:IsValidLivingHuman() and ent:IsPlayer() and ent:Team() == TEAM_ZOMBIE then
 		local applier = self.Applier
 
 		if applier:IsValidLivingHuman() then
 			applier.DefenceDamage = (applier.DefenceDamage or 0) + dmgfraction
 			applier.PointQueue = self.Applier.PointQueue + points * 1.15
+		    end
 		end
 	end
 end
