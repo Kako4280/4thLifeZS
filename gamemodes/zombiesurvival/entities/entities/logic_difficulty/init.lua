9cb9876df578aa802104fdb80dc32a986da0609f
Lightweasel <51730197+Lightweasel@users.noreply.github.com>
2021-10-02 09:25:56 -0700

    FGD option to change number of sigils a map has

--- gamemodes/zombiesurvival/entities/entities/logic_difficulty/init.lua
+++ gamemodes/zombiesurvival/entities/entities/logic_difficulty/init.lua
@@ -6,5 +6,11 @@ function ENT:AcceptInput(name, activator, caller, args)
 		GAMEMODE.ZombieDamageMultiplier = tonumber(args) or 1
 	elseif name == "setzombiespeedmultiplier" then
 		GAMEMODE.ZombieSpeedMultiplier = tonumber(args) or 1
+	elseif name == "setnumberofsigils" then
+		GAMEMODE.MaxSigils = tonumber(args) or 3
+	--[[elseif name == "barricadehealthmultiplier" then
+
+        will work on this later maybe ((PROBABLY NOT LOL!!))
+		end]]--
 	end
 end
