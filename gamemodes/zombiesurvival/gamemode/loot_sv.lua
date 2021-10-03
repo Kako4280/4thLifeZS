f68a4dc0171b786a5aee84d5fdab7bda39bfe19f
Lightweasel <51730197+Lightweasel@users.noreply.github.com>
2021-10-03 07:40:59 -0700

    add honeydipper to loot_sv

--- gamemodes/zombiesurvival/gamemode/loot_sv.lua
+++ gamemodes/zombiesurvival/gamemode/loot_sv.lua
@@ -152,7 +152,10 @@ local drops_regular_weapon_tier1 = {
 	[103] = {"weapon_zs_medicalkit", 1000},
 	[104] = {"weapon_zs_medicalkit_q1", 200},
 	[105] = {"weapon_zs_medicalkit_q2", 50},
-	[106] = {"weapon_zs_medicalkit_q3", 10}
+	[106] = {"weapon_zs_medicalkit_q3", 10},
+	[107] = {"weapon_zs_honeydipper", 200},
+	[108] = {"weapon_zs_honeydipper", 50},
+	[109] = {"weapon_zs_honeydipper", 10}
 }
 
 --Item, Weight
