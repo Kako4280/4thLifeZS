2f176a42d4d80fcdc65d9cd020752fe177a8395b
Lightweasel <51730197+Lightweasel@users.noreply.github.com>
2021-10-21 19:40:44 -0700

    recode parts of the zenith

---
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_zenith/cl_init.lua
@@ -0,0 +1,52 @@
+INC_CLIENT()
+
+SWEP.ViewModelFOV = 65
+SWEP.ShowViewModel = false
+SWEP.ShowWorldModel = false
+
+SWEP.VElements = {
+    ["zenith_guard"] = { type = "Model", model = "models/props_vehicles/tire001c_car.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3.45, 1.539, -5.2), angle = Angle(-1, 90, 0), size = Vector(0.2, 0.119, 0.119), color = Color(255, 215, 0, 255), surpresslightning = false, material = "phoenix_storms/gear", skin = 0, bodygroup = {} },
+    ["zenith_pommel"] = { type = "Model", model = "models/hunter/misc/cone4x2mirrored.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(2.619, 1.399, 2.9), angle = Angle(6, 0, 0), size = Vector(0.009, 0.009, 0.009), color = Color(255, 215, 0, 255), surpresslightning = false, material = "phoenix_storms/gear", skin = 0, bodygroup = {} },
+    ["zenith_guard_spike_small2"] = { type = "Model", model = "models/props_trainstation/trainstation_ornament002.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(2.5, 1.549, -6.25), angle = Angle(145, 0, 0), size = Vector(0.05, 0.025, 0.1), color = Color(255, 215, 0, 255), surpresslightning = false, material = "phoenix_storms/gear", skin = 0, bodygroup = {} },
+    ["zenith_guard_spike_small1"] = { type = "Model", model = "models/props_trainstation/trainstation_ornament002.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(4.75, 1.549, -6.25), angle = Angle(-130, 0, 0), size = Vector(0.05, 0.025, 0.1), color = Color(255, 215, 0, 255), surpresslightning = false, material = "phoenix_storms/gear", skin = 0, bodygroup = {} },
+    ["zenith_blade"] = { type = "Model", model = "models/xqm/wingtip1.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(5.03, 1.549, -20.085), angle = Angle(6, 0, 90), size = Vector(0.1, 1, 0.85), color = Color(75, 0, 130, 255), surpresslightning = false, material = "phoenix_storms/grey_chrome", skin = 0, bodygroup = {} },
+    ["zenith_grip"] = { type = "Model", model = "models/props_c17/signpole001.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3.299, 1.5, -3.75), angle = Angle(6, 0, -1), size = Vector(0.449, 0.449, 0.059), color = Color(75, 0, 130, 255), surpresslightning = false, material = "phoenix_storms/grey_chrome", skin = 0, bodygroup = {} },
+    ["zenith_guard_spike_large2"] = { type = "Model", model = "models/props_trainstation/trainstation_ornament002.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(2, 1.524, -5.22), angle = Angle(95, 0, 0), size = Vector(0.075, 0.025, 0.15), color = Color(255, 215, 0, 255), surpresslightning = false, material = "phoenix_storms/gear", skin = 0, bodygroup = {} },
+    ["zenith_guard_spike_large1"] = { type = "Model", model = "models/props_trainstation/trainstation_ornament002.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(5, 1.549, -5), angle = Angle(-85, 0, 0), size = Vector(0.075, 0.025, 0.15), color = Color(255, 215, 0, 255), surpresslightning = false, material = "phoenix_storms/gear", skin = 0, bodygroup = {} },
+    ["zenith_cutaway"] = { type = "Model", model = "models/xqm/wingtip1.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3.75, 1.549, -7.901), angle = Angle(6, 0, -90), size = Vector(0.1, 0.1, 0.85), color = Color(75, 0, 130, 255), surpresslightning = false, material = "phoenix_storms/grey_chrome", skin = 0, bodygroup = {} }
+}
+SWEP.WElements = {
+        ["zenith_guard"] = { type = "Model", model = "models/props_vehicles/tire001c_car.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3, 1.85, -5), angle = Angle(-5, 0, 0), size = Vector(0.2, 0.119, 0.119), color = Color(255, 215, 0, 255), surpresslightning = false, material = "phoenix_storms/gear", skin = 0, bodygroup = {} },
+        ["zenith_pommel"] = { type = "Model", model = "models/hunter/misc/cone4x2mirrored.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3.75, 1.2, 3.25), angle = Angle(-5.5, 0, -5), size = Vector(0.009, 0.009, 0.009), color = Color(255, 215, 0, 255), surpresslightning = false, material = "phoenix_storms/gear", skin = 0, bodygroup = {} },
+        ["zenith_guard_spike_small2"] = { type = "Model", model = "models/props_trainstation/trainstation_ornament002.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(2.95, 0.949, -6.25), angle = Angle(130, -90, 0), size = Vector(0.05, 0.025, 0.1), color = Color(255, 215, 0, 255), surpresslightning = false, material = "phoenix_storms/gear", skin = 0, bodygroup = {} },
+        ["zenith_guard_spike_large2"] = { type = "Model", model = "models/props_trainstation/trainstation_ornament002.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3, 0.5, -5.1), angle = Angle(95, -90, -3), size = Vector(0.075, 0.025, 0.15), color = Color(255, 215, 0, 255), surpresslightning = false, material = "phoenix_storms/gear", skin = 0, bodygroup = {} },
+        ["zenith_blade"] = { type = "Model", model = "models/xqm/wingtip1.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(1.72, 3.164, -19.66), angle = Angle(5, -90, 85), size = Vector(0.1, 1, 0.85), color = Color(75, 0, 130, 255), surpresslightning = false, material = "phoenix_storms/grey_chrome", skin = 0, bodygroup = {} },
+        ["zenith_grip"] = { type = "Model", model = "models/props_c17/signpole001.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3.099, 1.75, -3.5), angle = Angle(-5.5, 0, -4.5), size = Vector(0.449, 0.449, 0.059), color = Color(75, 0, 130, 255), surpresslightning = false, material = "phoenix_storms/grey_chrome", skin = 0, bodygroup = {} },
+        ["zenith_cutaway"] = { type = "Model", model = "models/xqm/wingtip1.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(2.799, 2.099, -7.5), angle = Angle(-5, 90, -85), size = Vector(0.1, 0.1, 0.85), color = Color(75, 0, 130, 255), surpresslightning = false, material = "phoenix_storms/grey_chrome", skin = 0, bodygroup = {} },
+        ["zenith_guard_spike_large1"] = { type = "Model", model = "models/props_trainstation/trainstation_ornament002.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3, 3, -4.901), angle = Angle(-85, -161.111, -74.445), size = Vector(0.075, 0.025, 0.15), color = Color(255, 215, 0, 255), surpresslightning = false, material = "phoenix_storms/gear", skin = 0, bodygroup = {} },
+        ["zenith_guard_spike_small1"] = { type = "Model", model = "models/props_trainstation/trainstation_ornament002.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(2.849, 3, -6), angle = Angle(-120, -85, 0), size = Vector(0.05, 0.025, 0.1), color = Color(255, 215, 0, 255), surpresslightning = false, material = "phoenix_storms/gear", skin = 0, bodygroup = {} }
+}
+
+local colWhite = Color(220, 220, 220, 230)
+local colBG = Color(16, 16, 16, 90)
+function SWEP:DrawHUD()
+    if GetConVar("crosshair"):GetInt() ~= 1 then return end
+	self:DrawCrosshairDot()
+    
+    local screenscale = BetterScreenScale()
+    
+    local wid, hei = 180 * screenscale, 64 * screenscale
+	local x, y = ScrW() - wid - screenscale * 128, ScrH() - hei - screenscale * 72
+	local spare = self:GetPrimaryAmmoCount()
+
+	local yy = ScrH() - hei * 2 - screenscale * 84
+
+    draw.RoundedBox(12, x, y * 0.99, wid, hei, colBG)
+    if self:GetElementalState() == 0 then
+		draw.SimpleTextBlurry("Pulse", "ZSHUDFont", x + wid * 0.5, yy * 1.11, colWhite, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+	elseif self:GetElementalState() == 1 then
+		draw.SimpleTextBlurry("Fire", "ZSHUDFont", x + wid * 0.5, yy * 1.11, colWhite, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+	elseif self:GetElementalState() == 2 then
+		draw.SimpleTextBlurry("Ice", "ZSHUDFont", x + wid * 0.5, yy * 1.11, colWhite, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+    end
+end
