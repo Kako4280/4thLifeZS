cbc8f641629cc0ac2f30e6b728ffc35a98cab92e
Lightweasel <51730197+Lightweasel@users.noreply.github.com>
2021-10-31 11:16:13 -0700

    upload great hammer explosion effect + killicon

--- gamemodes/zombiesurvival/entities/effects/shockwave.lua
+++ gamemodes/zombiesurvival/entities/effects/shockwave.lua
@@ -1,50 +1,44 @@
 function EFFECT:Init(effectdata)
 	local pos = effectdata:GetOrigin()
 	local normal = effectdata:GetNormal()
-
-	self.Emitter = ParticleEmitter(pos, true)
-	self.Emitter:SetNearClip(24, 32)
-	self.Emitter2 = ParticleEmitter(pos)
-	self.Emitter2:SetNearClip(24, 32)
 	
 	sound.Play("weapons/physcannon/energy_sing_explosion2.wav", pos, 80, math.random(100, 120))
 
+	local emitter = ParticleEmitter(pos)
+	emitter:SetNearClip(24, 32)
 
-	for i=1,3 do
-		local particle = self.Emitter:Add("effects/select_ring", pos)
+	for i=1, 3 do
+		local particle = emitter:Add("effects/select_ring", pos)
 		particle:SetDieTime(0.1 + i * 0.1)
 		particle:SetColor(255,255,255)
 		particle:SetStartAlpha(255)
 		particle:SetEndAlpha(0)
 		particle:SetStartSize(0)
-		particle:SetEndSize(150)
+		particle:SetEndSize(50)
 		particle:SetAngles(normal:Angle())
-		particle = self.Emitter:Add("effects/select_ring", pos)
-		particle:SetDieTime(0.2 + i * 0.1)
-		particle:SetColor(255,255,255)
-		particle:SetStartAlpha(255)
-		particle:SetEndAlpha(0)
-		particle:SetStartSize(0)
-		particle:SetEndSize(180)
-		particle:SetAngles(normal:Angle())
+		local particle2 = emitter:Add("effects/select_ring", pos)
+		particle2:SetDieTime(0.2 + i * 0.1)
+		particle2:SetColor(255,255,255)
+		particle2:SetStartAlpha(255)
+		particle2:SetEndAlpha(0)
+		particle2:SetStartSize(0)
+		particle2:SetEndSize(75)
+		particle2:SetAngles(normal:Angle())
 	end
-	for i=1,100 do
-		local particle = self.Emitter2:Add("effects/splash2", pos)
-		particle:SetDieTime(0.4)
+	for i=1, 50 do
+		local particle = emitter:Add("effects/splash2", pos)
+		particle:SetDieTime(0.2)
 		particle:SetColor(255,255,255)
 		particle:SetStartAlpha(255)
 		particle:SetEndAlpha(0)
-		particle:SetStartSize(4)
-		particle:SetEndSize(4)
+		particle:SetStartSize(3)
+		particle:SetEndSize(3)
 		particle:SetStartLength(30)
 		particle:SetEndLength(30)
-		particle:SetVelocity(VectorRand():GetNormal() * math.random(450,600))
+		particle:SetVelocity(VectorRand():GetNormal() * math.random(300,450))
 	end
 
-	--self.Emitter:Finish()
-	self.Emitter2:Finish()
-	
-	ExplosiveEffect(pos, 160, 40, DMGTYPE_GENERIC)
+	emitter:Finish() emitter = nil collectgarbage("step", 64)
 end
 
 function EFFECT:Think()
