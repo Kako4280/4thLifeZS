bba08a6b335fe232b5f55ac6340c4d9b6f68677a
Kako <41034193+Nebos4280@users.noreply.github.com>
2021-11-24 16:34:59 -0600

    Coded a teams menu...

--- gamemodes/flood/gamemode/client/vgui/flood_shopmenu_teams.lua
+++ gamemodes/flood/gamemode/client/vgui/flood_shopmenu_teams.lua
@@ -2,6 +2,18 @@ local x = ScrW()
 local y = ScrH()
 local g_ToolMenu = {}
 
+surface.CreateFont( "PlayerNameFont", {
+	font = "Arial",
+	size = 36,
+	weight = 1500
+})
+
+surface.CreateFont( "OtherInfoFont", {
+	font = "Arial",
+	size = 20,
+	weight = 900
+})
+
 local PANEL = {}
 function MakepTeamMenu()
 	local screenscale = BetterScreenScale()
@@ -107,57 +119,134 @@ function MakepTeamMenu()
 	jointeampanel:Dock(FILL)
 	createteamsheet:AddSheet("Join A Team", jointeampanel, "icon16/group_go.png")
 
-	local count = 2
-	local test = 0
-	local allteamtable = team.GetAllTeams()
-	PrintTable(allteamtable)
+	local allteamtable = team.GetAllTeams()	
+	local list = vgui.Create("DPanelList", jointeampanel)
+	list:SetSize((587 - 6) * screenscale, jointeampanel:GetTall() - (76 * screenscale))
+	list:SetPos(6, 0)
+	list:EnableVerticalScrollbar()
+	list:SetSpacing(-2)
+	list.Paint = function(self, w, h)
+		surface.SetDrawColor(0, 0, 0, 0)
+		surface.DrawRect(0, 0, w, h)
+	end
+	
+	local e = 0
+	local numPlayers = 0
+	
 	for k, v in pairs(allteamtable) do
-		count = count + 1
-
-		if team.Valid(count) then -- simple way to filter out default teams like spectator.
-			local teambuttoncolor = team.GetColor(count)
-			local isjoinabletest
-
-			if team.Joinable(count) then
-				isjoinabletest = "Joinable"
-			else
-				isjoinabletest = "Invite Only"
-			end
-
-			jointeamcollapsible = vgui.Create("DCollapsibleCategory", jointeampanel)
-			jointeamcollapsible:SetSize(400 * screenscale, 40 * screenscale)
-			jointeamcollapsible:Dock(TOP)
-			jointeamcollapsible:DockMargin(4,0,0,0)
-			jointeamcollapsible:SetLabel(team.GetName(count) .. "  Player Count:  " .. team.NumPlayers(count) .. " /  4" .. "  " .. isjoinabletest)
-			jointeamcollapsible:Toggle()
-			jointeamcollapsible.Paint = function(self, w, h)
-				draw.RoundedBox(0, 0, 0, w, h, Color(teambuttoncolor.r, teambuttoncolor.g, teambuttoncolor.b, 170))
-			end
-
-			collapsibleparentpanel = vgui.Create("DPanelList", jointeampanel)
-			collapsibleparentpanel:SetSpacing(5)
-			collapsibleparentpanel:EnableHorizontal(false)
-			collapsibleparentpanel:EnableVerticalScrollbar(true)
-			collapsibleparentpanel.Paint = function(self, w, h)
-				draw.RoundedBox(0, 0, 0, w, h, Color(24, 24, 24, 255))
-			end
-			jointeamcollapsible:SetContents(collapsibleparentpanel)
-
-			if team.Joinable(count) == true then
-				collapsiblebutton = vgui.Create("DButton", jointeampanel)
-				collapsiblebutton:SetSize(400 * screenscale, 20 * screenscale)
-				collapsiblebutton:SetText("JOIN TEAM")
-				collapsiblebutton.Paint = function(self, w, h) 
-					draw.RoundedBox(0, 0, 1, w, h, Color(0, 0, 0, 0)) 
-					draw.RoundedBox(0, 0, 0, w, h, Color(64, 64, 64, 255))
+		numPlayers = team.NumPlayers(k)
+		
+		if numPlayers > 0 then
+			if team.Valid(k) and k > 2 and k < 1001 then -- simple way to filter out default teams like spectator.
+				local teambuttoncolor = team.GetColor(k)
+				
+				local teamBox = vgui.Create("DPanel", list)
+				teamBox:SetSize((574 - 6) * screenscale, (40 * screenscale) + (78 * math.min(numPlayers, 2)))
+				teamBox:SetPos(0, 38 * screenscale * e)
+				teamBox.Paint = function(self, w, h)
+					surface.SetDrawColor(0, 0, 0, 0)
+					surface.DrawRect(0, 0, w, h)
 				end
-				collapsibleparentpanel:AddItem(collapsiblebutton)
-		    end
+				
+				local teamTitle = vgui.Create("DPanel", teamBox)
+				teamTitle:SetSize((574 - 6) * screenscale, 40 * screenscale)
+				teamTitle:SetPos(0, 0)
+				teamTitle.Paint = function(self, w, h)
+					surface.SetDrawColor(team.GetColor(k))
+					surface.DrawRect(0, 0, w, h)
+					surface.SetDrawColor(255, 255, 255, 255)
+					surface.DrawOutlinedRect(0, 0, w, h, 2)
+				end
+				
+				
+				local teamText = vgui.Create("DLabel", teamTitle)
+				teamText:SetText(team.GetName(k))
+				teamText:SizeToContents()
+				teamText:Center()
+				teamText:SetTextColor(Color(0, 0, 0, 255))
+				local color = team.GetColor(k)
+				if ((color.r + color.g + color.b) / 3) < 127 then
+					teamText:SetTextColor(Color(255, 255, 255, 255))
+				end
+				
+				local players = team.GetPlayers(k)
+				for i = 1, math.min(numPlayers + 1, 4) do
+					if i <= numPlayers then
+						local playerBox = vgui.Create("DPanel", teamBox)
+						playerBox:SetSize((574 - 4) * screenscale / 2, 80 * screenscale)
+						playerBox:SetPos((1 - math.ceil((i / 2)-math.floor(i / 2))) * (572 - 6) * screenscale / 2, math.floor((i - 1) / 2) * 78 + 38)
+						playerBox.Paint = function(self, w, h)
+							surface.SetDrawColor(200, 200, 200, 255)
+							surface.DrawRect(0, 0, w, h)
+							surface.SetDrawColor(255, 255, 255, 255)
+							surface.DrawOutlinedRect(0, 0, w, h, 2)
+						end
+						
+						if IsValid(players[i]) then
+							local avatarFrame = vgui.Create("DPanel", playerBox)
+							avatarFrame:SetSize(76 * screenscale, 76 * screenscale)
+							avatarFrame:SetPos(2, 2)
+							avatarFrame.Paint = function(self, w, h)
+								surface.SetDrawColor(0, 0, 0, 0)
+								surface.DrawRect(0, 0, w, h)
+							end	
+							
+							local infoFrame = vgui.Create("DPanel", playerBox)
+							infoFrame:SetSize(((574 - 4) * screenscale / 2) - (80 * screenscale), 76 * screenscale)
+							infoFrame:SetPos(78 * screenscale, 2)
+							infoFrame.Paint = function(self, w, h)
+								surface.SetDrawColor(0, 0, 0, 0)
+								surface.DrawRect(0, 0, w, h)
+							end	
+							
+						
+							local avatar = vgui.Create("AvatarImage", avatarFrame)
+							avatar:SetSize(76 * screenscale, 76 * screenscale)
+							avatar:SetPos(0, 0)
+							avatar:SetPlayer(players[i], 74)
+							
+							local name = vgui.Create("DLabel", infoFrame)
+							name:SetText(players[i]:GetName())
+							name:SetFont("PlayerNameFont")
+							name:SizeToContents()
+							name:SetTextColor(Color(0, 0, 0, 255)) --Set this to blue (0, 0, 255) if person is party leader.
+							name:SetPos((infoFrame:GetWide() - name:GetWide()) / 2, 12)
+							
+							local level = vgui.Create("DLabel", infoFrame)
+							level:SetText("Level " .. CalculateLevel(players[i]))
+							level:SetFont("OtherInfoFont")
+							level:SizeToContents()
+							level:SetTextColor(Color(0, 0, 0, 255))
+							level:SetPos((infoFrame:GetWide() - level:GetWide()) / 2, name:GetTall() + 8)
+						end
+					elseif team.Joinable(k) then
+						local joinBox = vgui.Create("DPanel", teamBox)
+						joinBox:SetSize((574 - 4) * screenscale / 2, 80 * screenscale)
+						joinBox:SetPos((1 - math.ceil((i / 2)-math.floor(i / 2))) * (572 - 6) * screenscale / 2, math.floor((i - 1) / 2) * 78 + 38)
+						joinBox.Paint = function(self, w, h)
+							surface.SetDrawColor(200, 225, 200, 255)
+							surface.DrawRect(0, 0, w, h)
+							surface.SetDrawColor(255, 255, 255, 255)
+							surface.DrawOutlinedRect(0, 0, w, h, 2)
+						end
+						
+						local joinButton = vgui.Create("DButton", joinBox)
+						joinButton:SetText("Join")
+						joinButton:SetPos(0, 0)
+						joinButton:SetSize(80, 32)
+						joinButton:Center()
+						joinButton.DoClick = function()
+							--RunConsoleCommand
+						end
+					end
+				end
+				
+				list:AddItem(teamBox)
+				
+				e = e + 1
+			end
 		end
 	end
-
-
-
 
 	local manageteampanel = vgui.Create("DPanel", createteamsheet)
 	manageteampanel:SetSize(587 * screenscale, 300 * screenscale)
